a14193fe6c1c120765ee03921d2467d1a6bcb5ad
Ryan Harrington <Krooked590@gmail.com>
2025-06-17 17:02:09 -0700

    first commit

---
+++ krookedlilly-web/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../logo.svg"
+
+export const Home: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
