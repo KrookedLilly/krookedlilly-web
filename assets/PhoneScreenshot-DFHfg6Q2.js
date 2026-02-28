627f4fbbfb4ebc12d9aad194aa6e4bee84039908
Ryan Harrington <Krooked590@gmail.com>
2026-02-27 23:31:34 -0800

    Updates

---
+++ assets/PhoneScreenshot-DFHfg6Q2.js
@@ -0,0 +1,16 @@
+import{c as d,j as o}from"./index-Dd4N0JW7.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],v=d("chevron-left",l);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],u=d("chevron-right",i);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M6 9H4.5a2.5 2.5 0 0 1 0-5H6",key:"17hqa7"}],["path",{d:"M18 9h1.5a2.5 2.5 0 0 0 0-5H18",key:"lmptdp"}],["path",{d:"M4 22h16",key:"57wxv0"}],["path",{d:"M10 14.66V17c0 .55-.47.98-.97 1.21C7.85 18.75 7 20.24 7 22",key:"1nw9bq"}],["path",{d:"M14 14.66V17c0 .55.47.98.97 1.21C16.15 18.75 17 20.24 17 22",key:"1np0yb"}],["path",{d:"M18 2H6v7a6 6 0 0 0 12 0V2Z",key:"u46fv3"}]],b=d("trophy",c);function f({src:r,alt:a,className:t="",bezel:s=!1,objectPosition:h="top",onClick:e}){const n=o.jsx("div",{className:"relative w-full overflow-hidden rounded-lg",style:{paddingBottom:"216.67%"},children:o.jsx("img",{src:r,alt:a,className:"absolute inset-0 w-full h-full object-cover",style:{objectPosition:h},draggable:!1})});return s?o.jsx("div",{className:`inline-block rounded-[2rem] border-[6px] border-white/10 bg-black p-[3px] shadow-xl ${t}`,onClick:e,role:e?"button":void 0,tabIndex:e?0:void 0,children:o.jsx("div",{className:"overflow-hidden rounded-[1.5rem]",children:n})}):o.jsx("div",{className:`inline-block overflow-hidden rounded-lg ${t}`,onClick:e,role:e?"button":void 0,tabIndex:e?0:void 0,children:n})}export{v as C,f as P,b as T,u as a};
